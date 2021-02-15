30802d4db220e83da7d7a9250c373d8df9fb17e9
silva-kleyton <silva-kleyton@hotmail.com>
2021-02-15 16:32:22 -0300

    heroku docker

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,10 @@ import ShortenerLinkRouter from "./ShortenerLink.router";
 
 const routes = Router();
 
+routes.get("/", (req, res) => {
+  return res.status(200).send("<h1>Teste</h1>");
+});
+
 routes.use("", ShortenerLinkRouter);
 
 export default routes;
