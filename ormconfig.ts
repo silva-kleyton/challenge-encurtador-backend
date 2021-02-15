941ed896c82a07cf5facf6206a65c86261130367
silva-kleyton <silva-kleyton@hotmail.com>
2021-02-15 20:26:03 -0300

    ok

--- ormconfig.ts
+++ ormconfig.ts
@@ -4,10 +4,10 @@ import "dotenv/config";
 export default {
   type: "postgres",
   host: "ec2-52-23-190-126.compute-1.amazonaws.com",
-  port: process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE,
+  port: "5432",
+  username: "mxmsnxbjuydcaa",
+  password: "8dc73db070581a3de6e4ba96d73e8b539e3a96e77c7825203f70dd28012c58f6",
+  database: "dd6vtp2mkoolgf",
   entities: ["./src/models/*.ts"],
   migrations: ["./src/database/migrations/*.ts"],
   cli: {
