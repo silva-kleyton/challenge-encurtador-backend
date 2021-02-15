2140e675d4cd6423acc2109c34074a0c8e049bc1
silva-kleyton <silva-kleyton@hotmail.com>
2021-02-15 20:19:41 -0300

    ok

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,8 +3,7 @@ import "dotenv/config";
 //
 export default {
   type: "postgres",
-  host:
-    process.env.RUN_DOCKER === "true" ? "host.docker.internal" : "localhost",
+  host: "ec2-52-23-190-126.compute-1.amazonaws.com",
   port: process.env.DB_PORT,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
